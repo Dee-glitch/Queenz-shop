e4ec125812ec1611a830d429bde203893eebaac7
Deedee <71313732+Dee-glitch@users.noreply.github.com>
2021-06-15 18:03:00 +0200

    Update .env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyA5dUzFCQTe0hZ3T9qcsd92Nr_QqL5JZv4',
-  authDomain: 'queenzz.firebaseapp.com',
-  projectId: 'queenzz',
-  storageBucket: 'queenzz.appspot.com',
-  messagingSenderId: '14316713533',
-  appId: '1:14316713533:web:d352009d32763af8b15c54',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(config);
